2fbf13a520ae0aabdcf961a69e9252c9d47d1624
hatuandung <36006265+hatuandung@users.noreply.github.com>
2018-05-04 20:31:50 +0700

    a
    a

--- Example/decision.js
+++ Example/decision.js
@@ -6,6 +6,7 @@
 const decide = (history = []) => {
   var pre = history.length - 1;
   var myChoice  = true;
+  var count = 0;
 for(var i = 0; i < pre; i++){
   if(history[i].theirs == true){
     myChoice = false;
@@ -13,6 +14,15 @@ for(var i = 0; i < pre; i++){
   if(history[i].theirs == false){
     myChoice = true;
   }
+  if(history[i].yours == false ){
+    count = count + 1;
+  }
+  if(history[i].yours == false && count > 50){
+    myChoice = true;
+  }
+  //if(p1Answer == true )
+  
+  
 }
   
 
